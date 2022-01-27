e5068e1cd61a58350caece9f5f0447f663d93ec9
Mihai Dornescu <dornescu@gmail.com>
2022-01-27 12:22:54 +0200

    inscriere firma mobile show error

--- FC/cypress/integration/inscriere.spec.js
+++ FC/cypress/integration/inscriere.spec.js
@@ -21,6 +21,18 @@ describe('register company mobile', () => {
 		cy.get(':nth-child(6) > .form-control').type(Cypress.env('phone'));
 		cy.get(':nth-child(7) > .form-control').type(Cypress.env('phone'));
 	});
+	it.only('company info emty show error', function () {
+		cy.viewport('iphone-xr');
+		cy.get(':nth-child(3) > :nth-child(1) > .form-control').click();
+		cy.get(':nth-child(3) > :nth-child(2) > .form-control').click();
+		cy.get(':nth-child(3) > :nth-child(3) > .form-control').click();
+		cy.get(':nth-child(4) > .form-control').select(4);
+		cy.get(':nth-child(5) > .form-control').click();
+		cy.get(':nth-child(6) > .form-control').click();
+		cy.get(':nth-child(7) > .form-control').click();
+		cy.get('.btnM ').click();
+	});
+
 	it('ANCPI imput', function () {
 		cy.get(':nth-child(4) > .row > :nth-child(1) > .form-control').type(Cypress.env('name'));
 		cy.get(':nth-child(4) > .row > :nth-child(2) > .form-control').type(Cypress.env('email'));
@@ -37,7 +49,7 @@ describe('register company mobile', () => {
 		cy.get('#terms-and-conditions').click();
 	});
 	// fixme not closing modal
-	it.only('should close modal after it opens it', function () {
+	it('should close modal after it opens it', function () {
 		cy.get('.cc-btn').click();
 		cy.get('#terms-and-conditions').click();
 		cy.get('.close > span').click({force:true});
